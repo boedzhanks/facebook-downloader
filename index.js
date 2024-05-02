beda9ee47000c76854c9e7691e7f3634ee1d796a
Boedzhanks <hardiansyahramadhani084@gmail.com>
2024-05-02 07:23:28 +0700

    fix

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const { default: axios } = require('axios');
 
+url = `https://fb.watch/r2GFy0tGQA/`
+
 exports.fbdown = async (url) => {
 try {
 const res = await axios('https://x2download.app/api/ajaxSearch/facebook',{
 			method: "POST",
 			data: "q=" + encodeURIComponent(url)
   })
-    console.log(res.data);
-    return res.data
+    let p = res.data
+    return {
+      status: p.status,
+      author: "Boedzhanks",
+      title: p.title,
+      duration: p.duration,
+      thumbnail: p.thumbnail,
+      link: p.links
+    }
 } catch (err) {
   return err
 }
